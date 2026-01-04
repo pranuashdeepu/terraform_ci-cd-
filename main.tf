d82f0d0c2f97d60354d1d854cbc985b30ac4ef6d
pranuashdeepu <praneethreddynallapareddy2588@gmail.com>
2026-01-04 21:54:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0f88e80871fd81e91"
-    instance_type = "t2.micro"
-    tags = {
-      Name = "vvvnaresh"
-    }
+  ami           = "ami-00ca570c1b6d79f36"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "pranu"
+  }
 }
